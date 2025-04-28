9ad3075689c18fcb8993e1bcae4e6629a0a97b22
wpiskaa <hafizzkurniawan19@gmail.com>
2025-04-28 11:04:23 +0700

    memanggil

--- class2.cpp
+++ class2.cpp
@@ -1,28 +1,38 @@
-#include  <iostream>
+#include <iostream>
 using namespace std;
 
-class PersegiPanjang{
-    private:
+class PersegiPanjang
+{
+private:
     int Panjang;
     int Lebar;
     int Luas;
 
-    public:
-    void inputData(){
+public:
+    void inputData()
+    {
         cout << "Masukkan Panjang :";
         cin >> Panjang;
         cout << "Masukkan Lebar :";
         cin >> Lebar;
     }
-    void outputData(){
+    void hitungLuas()
+    {
+    Luas =
+        Panjang *Lebar;
+    }
+    
+    void outputData()
+    {
         cout << "Tampilkan Luas :" << Luas << endl;
     }
 
-void hitungLuas(){
-    Luas : Panjang*Lebar;
-}
-
+  
 };
-int main(){
-    
+int main()
+{
+    PersegiPanjang pp;
+    pp.inputData();
+    pp.hitungLuas();
+    pp.outputData();
 }
