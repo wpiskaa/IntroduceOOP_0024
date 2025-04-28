8ac7a58ab9bf024a32d93ef895d1c27614b51720
wpiskaa <hafizzkurniawan19@gmail.com>
2025-04-28 10:22:49 +0700

    memanggil mobil

--- class.cpp
+++ class.cpp
@@ -11,12 +11,17 @@ private:
     int harga_mobil;
 
 public:
-void inputData(){
-    cout << "Masukkan nama mobil: ";
-    cin >> nama_mobil;
-    cout << "Masukkan tipe mobil: ";
-    cin >> tipe_mobil;
-    cout << "Masukkan harga mobil: ";
-    cin >> harga_mobil;
-}
+    void inputData()
+    {
+        cout << "Masukkan nama mobil: ";
+        cin >> nama_mobil;
+        cout << "Masukkan tipe mobil: ";
+        cin >> tipe_mobil;
+        cout << "Masukkan harga mobil: ";
+        cin >> harga_mobil;
+    }
 };
+int main(){
+    Mobil Pejabat;
+    
+}
