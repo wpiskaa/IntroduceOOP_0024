aacf3af622e36f068f69c1042132225b2615d50a
wpiskaa <hafizzkurniawan19@gmail.com>
2025-04-28 10:18:04 +0700

    cout data

--- class.cpp
+++ class.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Mobil{
-    public
+class Mobil
+{
+public:
+    string nama_mobil;
+    string tipe_mobil;
+
+private:
+    int harga_mobil;
+
+public:
+void inputData(){
+    cout << "Masukkan nama mobil: ";
+    cout << "Masukkan tipe mobil: ";
 }
+};
